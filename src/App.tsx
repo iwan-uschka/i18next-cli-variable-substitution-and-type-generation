25c403dd9dba9677344074cae381b88d220b4bcd
Christoph Wanja <christoph.wanja@bitgrip.de>
2026-03-16 09:21:04 +0100

    Add comment

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ function App() {
   return (
     <>
       <div>
+        {/* TSC doesn't complain */}
         {i18n.t("message[messageKey1]", {
           variable: "value",
         })}
       </div>
 
       <div>
+        {/* TSC complains due to the whitespaces around the variable name in the message `Message 2 with {{ variable }}` */}
         {i18n.t("message[messageKey2]", {
           variable: "value",
         })}
